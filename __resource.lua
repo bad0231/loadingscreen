28eb62a15f35bbadce8368b6a86dd87a477d01fd
bad0231 <60315325+bad0231@users.noreply.github.com>
2020-01-26 12:41:19 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+description 'LoadingScreen!'
+
+files {
+    'index.html',
+    'cool/style.css',
+    'js/index.js',
+    'music/music.ogg',
+    'fivem.jpg',
+    'jscript.js',
+    'logo.png',
+    'style.css',
+
+}
+
+loadscreen 'index.html'
